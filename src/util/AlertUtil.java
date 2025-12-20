8b7a5154035fb9e5872d7ffb50d3120092036ba5
AK <aksingh252007@gmail.com>
2025-12-20 11:12:14 +0530

    Alert

---
+++ src/util/AlertUtil.java
@@ -0,0 +1,11 @@
+package util;
+
+import javafx.scene.control.Alert;
+
+public class AlertUtil {
+    public static void error(String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(msg);
+        alert.show();
+    }
+}
